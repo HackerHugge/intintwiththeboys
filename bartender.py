55b18cd7f72bdbf3251a0760a265fb7f2dd1b0a3
HugoAsztely <hugo.asztely.4609@student.uu.se>
2023-12-08 14:46:43 +0100

    text

--- bartender.py
+++ bartender.py
@@ -1,23 +1,27 @@
-# INTINT WITH THE BOYS
-# HUGO ASZTELY, SIMON OLOFSSON & JOEL TINGWALL
-# Code outline for a hardcoded furhat bartender with dialogues based on keywords
-
-# change to your directory in terminal where the python used is, and pip install all necessary modules and references
-# NECESSARY IMPORTS
-from time import sleep
-from furhat_remote_api import FurhatRemoteAPI
-from numpy.random import randint
-import speech_recognition as sr
+# Import necessary modules
+import cv2
+import numpy as np
 import random
+import speech_recognition as sr
+from furhat_remote_api import FurhatRemoteAPI
+from tensorflow import keras
+from time import sleep
 
+# Load the pre-trained emotion recognition model
+emotion_model = keras.models.load_model('emotion_recognition_model.h5')
+
+# Define a list of emotions for classification
+emotion_labels = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
+
+# Define Furhat parameters
 FURHAT_IP = "127.0.1.1"
-
 furhat = FurhatRemoteAPI(FURHAT_IP)
 furhat.set_led(red=100, green=50, blue=50)
 
 # Initialize the speech recognition recognizer
 recognizer = sr.Recognizer()
 
+# Define Furhat attributes
 FACES = {
     'Amany': 'Nazar'
 }
@@ -30,6 +34,7 @@ VOICES_NATIVE = {
     'Amany': 'AmanyNeural'
 }
 
+# Define Furhat gestures
 def idle_animation():
     furhat.gesture(name="GazeAway")
     gesture = {
@@ -38,13 +43,12 @@ def idle_animation():
                 "time": [0.33],
                 "persist": True,
                 "params": {
-                    "NECK_PAN": randint(-4, 4),
-                    "NECK_TILT": randint(-4, 4),
-                    "NECK_ROLL": randint(-4, 4),
+                    "NECK_PAN": random.randint(-4, 4),
+                    "NECK_TILT": random.randint(-4, 4),
+                    "NECK_ROLL": random.randint(-4, 4),
                 }
             }
         ],
-
         "class": "furhatos.gestures.Gesture"
     }
     furhat.gesture(body=gesture, blocking=True)
@@ -77,7 +81,6 @@ def LOOK_BACK(speed):
         "class": "furhatos.gestures.Gesture"
     }
 
-# DO NOT CHANGE
 def LOOK_DOWN(speed=1):
     return {
         "frames": [
@@ -115,55 +118,88 @@ def set_persona(persona):
 def bsay(line):
     furhat.say(text=line, blocking=True)
 
-# DIALOGUE KEYWORDS THATAMANI CAN RESPOND TO
-KEYWORD_CAN_I_GET_A = "can i get a"
-KEYWORD_WAIT = "wait" 
+# Define a mapping of emotions to responses
+emotion_responses = {
+    "angry": "I see you're angry. What can I do to help?",
+    "happy": "Glad you are feeling happy!",
+    "sad": "I'm here to listen. What's bothering you?",
+    "surprise": "You look surprised! What's going on?",
+    "disgust": "I sense some disgust. Can you tell me what's bothering you?",
+    "fear": "Don't be afraid. I'm here to assist you. What can I help you with?",
+    "neutral": "I'm here to assist you. How can I help?"
+}
+
+# DIALOGUE KEYWORDS THAT AMANI CAN RESPOND TO
+KEYWORD_CAN_I_GET_A = "can I get a"
+KEYWORD_WAIT = "wait"
 KEYWORD_LEAVING = "bye"
 
 # DIALOGUE CONSTANTS THAT AMANY SAYS UPON INTRO AND ENDING
-KEYWORD_GREET = "Hello client number 8. What can I get you?" # AMANI
-KEYWORD_BYE= "Okay, please come back, or don't. I literally cannot care less." # AMANI
+KEYWORD_GREET = "Hello! What can I get you?"  # AMANI
+KEYWORD_BYE = "Okay, please come back, or don't. I literally cannot care less."  # AMANI
 
 def analyze_keywords(user_response):
+    user_response = user_response.lower()
+
     keyword_responses = {
-        KEYWORD_CAN_I_GET_A: "Certainly, coming right up in ... calculating... six hours. Please transaction 0.00002 bitcoin.",
-        KEYWORD_WAIT: "Yes I am aware that service is a little slow. I dont have any arms to mix drinks with.",
+        KEYWORD_CAN_I_GET_A: "Certainly, coming right up in six hours. Please transaction 0.00002 bitcoin.",
+        KEYWORD_WAIT: "Yes, I am aware that service is a little slow. I don't have any arms to mix drinks with.",
     }
 
-    # RANDOM RESPONSES IF SENTENCE USER SAYS DOES NOT MATCH KEYWORD
     default_responses = [
         "I don't speak drunk. Try to improve your voice mechanics.",
         "What did you say?"
     ]
 
     for keyword, response in keyword_responses.items():
-        if keyword in user_response.lower():
+        if keyword.lower() in user_response:
             if keyword == KEYWORD_CAN_I_GET_A:
                 response_parts = response.split("Certainly,")
                 if len(response_parts) == 2:
-                    # Perform the "Wink" gesture after saying "Certainly,"
                     bsay("Certainly,")
                     furhat.gesture(name="Wink")
-                    sleep(0.3)  # Adjust the sleep time as needed
+                    sleep(0.3)
                     return response_parts[1].strip()
             elif keyword == KEYWORD_WAIT:
                 response_parts = response.split("little slow")
                 if len(response_parts) == 2:
-                    # Perform the "Thoughtful" gesture after saying "little slow"
-                    bsay("Yes I am aware that service is a little slow.")
-                    sleep(0.5)  # Adjust the sleep time as needed
+                    bsay("Yes, I am aware that service is a little slow.")
+                    sleep(0.5)
                     furhat.gesture(name="Thoughtful")
-                    sleep(0.5)  # Adjust the sleep time as needed
+                    sleep(0.5)
                     return response_parts[1].strip()
 
-    # If no keyword matched, select a random default response from default_responses
     random_default_response = random.choice(default_responses)
-    
-    # Perform the "Roll" gesture
     furhat.gesture(name="Roll")
-    
     return random_default_response
 
+# Function to recognize emotions in real-time from video feed
+def recognize_emotion_from_video():
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        input_frame = cv2.resize(frame, (48, 48))
+        input_frame = cv2.cvtColor(input_frame, cv2.COLOR_BGR2RGB)
+        input_frame = np.expand_dims(input_frame, axis=0)
+
+        emotion_predictions = emotion_model.predict(input_frame)
+        emotion_label = emotion_labels[emotion_predictions[0].argmax()]
+        print(emotion_predictions)
+        return emotion_label
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+# Function to analyze emotions and provide responses
+def analyze_emotion(emotion):
+    return emotion_responses.get(emotion.lower(), "I'm here to assist you. How can I help?")
+
+# Function to interact with the user
 def interact_with_user():
     set_persona('Amany')
     bsay(KEYWORD_GREET)
@@ -171,10 +207,11 @@ def interact_with_user():
     while True:
         user_response = listen_to_user()
 
+        # Capture the emotion only once
+        recognized_emotion = recognize_emotion_from_video()
+
         exit_phrases = [KEYWORD_LEAVING]
 
-        
-        # Check if any of the exit phrases are present in the user's response
         if any(phrase in user_response.lower() for phrase in exit_phrases):
             bsay(KEYWORD_BYE)
             furhat.gesture(name="Roll")
@@ -183,21 +220,28 @@ def interact_with_user():
         furhat.set_voice(name=VOICES_NATIVE['Amany'])
 
         response = analyze_keywords(user_response)
-
+        print(f"Amani's Keyword Response: {response}")  # Print Amani's response to the keyword
         bsay(response)
+        sleep(1)
 
+        emotion_response = analyze_emotion(recognized_emotion)
+        print(f"Recognized Emotion: {recognized_emotion}")  # Print the recognized emotion
+        print(f"Amani's Response: {emotion_response}")  # Print Amani's response to the emotion
+        bsay(emotion_response)  # Say the recognized emotion
+# Function to listen to user's speech
 def listen_to_user():
     with sr.Microphone() as source:
         print("Listening...")
         try:
             audio = recognizer.listen(source)
-            user_response = recognizer.recognize_google(audio)  # Recognize user's speech
+            user_response = recognizer.recognize_google(audio)
             print(f"User said: {user_response}")
             return user_response
         except sr.UnknownValueError:
-            return ""  # Return an empty string if speech couldn't be recognized
+            return ""
 
 if __name__ == '__main__':
-    interact_with_user()
-    idle_animation()
-
+    try:
+        interact_with_user()
+    finally:
+        idle_animation()
